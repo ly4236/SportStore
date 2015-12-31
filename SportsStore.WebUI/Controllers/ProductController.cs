72df515725b52bfdbbf877b03830217f583d2283
liyang <535133951@qq.com>
2015-12-31 17:30:06 +0800

    1

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -4,13 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using SportsStore.WebUI.Models;
 namespace SportsStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
-        public int PageSize = 4;
+        public int PageSize = 2;
 
         public ProductController(IProductRepository productRepository)
         {
@@ -18,12 +18,51 @@ namespace SportsStore.WebUI.Controllers
         }
 
 
-        public ViewResult List(int page = 1)
+        //        public ViewResult List(int page = 1)
+        //        {
+        //            ProductsListViewModel model = new ProductsListViewModel
+        //            {
+        //                Products = repository.Products
+        //.OrderBy(p => p.ProductID)
+        //.Skip((page - 1) * PageSize)
+        //.Take(PageSize),
+        //                PagingInfo = new PagingInfo
+        //                {
+        //                    CurrentPage = page,
+        //                    ItemsPerPage = PageSize,
+        //                    TotalItems = repository.Products.Count()
+        //                }
+        //            };
+        //            return View(model);
+
+        //        }
+
+        public ViewResult List(string category, int page = 1)
         {
-            return View(repository.Products
-            .OrderBy(p => p.ProductID)
-            .Skip((page - 1) * PageSize)
-            .Take(PageSize));
-        }
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
+                    .Where(p => category == null || p.Category ==
+                    category)
+                    .OrderBy(p => p.ProductID)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems =category == null ?
+                            repository.Products.Count() :
+                            repository.Products.Where(e => e.Category ==
+                            category).Count()
+                },
+                CurrentCategory = category
+            };
+            return View(model);
+        }
+
+
     }
+
+
 }
